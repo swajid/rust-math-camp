891b73652f6e6632a6a12a2cf4ee96e5568da9ba
swajid <80992660+swajid@users.noreply.github.com>
2023-12-11 11:02:14 -0500

    Create intro-linear-algebra.rs

---
+++ intro-linear-algebra.rs
@@ -0,0 +1,16 @@
+extern crate nalgebra as na;
+use na::{DMatrix, Matrix3x2, Const};
+
+use matrixcompare::comparators::{AbsoluteElementwiseComparator, ExactElementwiseComparator};
+use matrixcompare::compare_matrices;
+
+
+fn main() {
+    let k = Matrix3x3::new(1, 0, 0,
+                           0, 1, 0,
+                           0, 0, 1,);
+    let ident = DMatrix::identity(3, 3);
+    assert!(!k.isdentity())
+    assert!(!ident.isdentity())
+    assert_matrix_eq!(k, ident, comp = exact);
+}
